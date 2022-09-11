f725ab5b0bd32351e41d0b81662a9b5e1a0e6f9e
Howhowchen <debby8698@gmail.com>
2022-09-12 00:37:59 +0800

    feat: add error condition

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,7 +3,7 @@ const Url = require('../../models/Url')
 const shortenUrl = require('../../utils/shortenUrl')
 const router = express.Router()
 const port = process.env.PORT || 3000
-
+//let projectUrl =  `http://howhowshorturl/`
 let projectUrl =  `http://localhost:${port}/`
 router.get('/', (req, res) => {
   res.render('index')
@@ -11,22 +11,44 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
   const originUrl = req.body.originUrl.trim()
-  const shortUrl = shortenUrl()
-  const url = projectUrl + shortUrl
+  let copyUrl
 
-  return Url.create({ originUrl, shortUrl })
-    .then(() => {
-      res.render('copy', { url })
+  return Url.findOne({ originUrl })
+    .then(url => {
+      //  未重複
+      if (!url) {
+        const shortUrl = shortenUrl()
+        copyUrl = projectUrl + shortUrl
+        Url.create({ originUrl, shortUrl })
+        .then(() => {
+          return res.render('copy', { copyUrl, success: 1 })
+        }) 
+        .catch(error => console.log(error))
+      }
+      //  重複
+      copyUrl = projectUrl + url.shortUrl
+      res.render('copy', { copyUrl, success: 1 })
     })
     .catch(error => console.log(error))
 })
 
 router.get('/:shortUrl', (req, res) => {
   const shortUrl = req.params.shortUrl
+  let errorMsg
   return Url.findOne({ shortUrl })
     .lean()
     .then(url => {
-      res.redirect(url['originUrl'])
+      if (!url) {
+        errorMsg = 'The url is not found!'
+        return res.render('copy', { errorMsg, error: 1 })
+      }
+      
+      res.redirect(url.originUrl)
+    })
+    .catch(error => {
+      console.log(error)
+      errorMsg = error.message
+      res.render('copy', { errorMsg, error: 1 })
     })
 })
 
