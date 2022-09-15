82f8be12d53fe3b96b024ad6ff0eac2f59b6399d
howhow <debby8698@gmail.com>
2022-09-15 19:26:02 +0800

    refactor: modify copy.js EventListener

--- public/javascripts/copy.js
+++ public/javascripts/copy.js
@@ -1,6 +1,7 @@
 const copyBtn = document.querySelector('#copy-btn')
 const shortUrl = document.querySelector('#short-url')
-
-copyBtn.addEventListener('click', function onShortBtnClicked(event) {
-  navigator.clipboard.writeText(shortUrl.textContent)
-})
+try {
+  copyBtn.addEventListener('click', function onShortBtnClicked(event) {
+    navigator.clipboard.writeText(shortUrl.textContent)
+  })
+} catch {}
